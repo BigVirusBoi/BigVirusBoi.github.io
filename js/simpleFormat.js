669fe4cfa980cd1df3fea9e938facf8ce23171ae
BigVirusBoi <bigvirusboi@gmail.com>
2021-06-03 21:13:03 +0200

    hello
    i am definitely having fun..................................

---
+++ js/simpleFormat.js
@@ -0,0 +1,12 @@
+var copyright = "Copyright 2021-" + new Date().getFullYear() + " &copy; BigVirusBoi. All Rights Reserved";
+document.getElementById("copyright").innerHTML = copyright;
+
+/* FIX THIS
+$(function(){
+    var includes = $('[data-include]');
+    $.each(includes, function () {
+      var file = '../templates/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+});
+*/
